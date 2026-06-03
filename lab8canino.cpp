725f837585a7ce3774c27a77fd361f608b1d9af6
miazmaa <alc032731@gmail.com>
2026-06-03 18:23:04 -0400

    it bounces and rotates when it hits a wall but not like the original video. also uses a new png since i had to size down the original due to problems

--- lab8canino.cpp
+++ lab8canino.cpp
@@ -1,5 +1,6 @@
 #include <allegro5\allegro.h>
 #include <allegro5\allegro_image.h>
+#include <math.h>
 
 int main(int argc, char** argv) {
     const float FPS = 60;
@@ -13,8 +14,10 @@ int main(int argc, char** argv) {
     float fish_x = SCREEN_W / 2.0 - fish_SIZE / 2.0;
     float fish_y = SCREEN_H / 2.0 - fish_SIZE / 2.0;
     float fish_dx = -4.0, duck_dy = 4.0;
+    float angle = 0.0;
     bool redraw = true;
     bool pause = false; 
+    int flags = 0;
 
     ALLEGRO_BITMAP* ocean = NULL;
     ALLEGRO_BITMAP* fish = NULL;
@@ -22,8 +25,6 @@ int main(int argc, char** argv) {
     if (!al_init()) {
         return -1;
     }
-
-    // <-- 2. Initialize keyboard
     if (!al_install_keyboard()) {
         return -1;
     }
@@ -41,7 +42,7 @@ int main(int argc, char** argv) {
 
     al_init_image_addon();
     ocean = al_load_bitmap("ocean.png");
-    fish = al_load_bitmap("fish.png");
+    fish = al_load_bitmap("clownfish.png");
     al_convert_mask_to_alpha(fish, al_map_rgb(255, 0, 255));
 
     event_queue = al_create_event_queue();
@@ -55,7 +56,7 @@ int main(int argc, char** argv) {
 
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_register_event_source(event_queue, al_get_timer_event_source(timer));
-    al_register_event_source(event_queue, al_get_keyboard_event_source()); // <-- 3. Register keyboard
+    al_register_event_source(event_queue, al_get_keyboard_event_source()); 
 
     al_clear_to_color(al_map_rgb(0, 0, 0));
     al_flip_display();
@@ -67,13 +68,27 @@ int main(int argc, char** argv) {
 
         if (ev.type == ALLEGRO_EVENT_TIMER) {
             if (!pause) {
+                bool wall_touch = false;
+
                 if (fish_x < 0 || fish_x > SCREEN_W - fish_SIZE) {
                     fish_dx = -fish_dx;
+                    angle = 0.0;
+                    wall_touch = true; //boolean appears in these now to check collision
                 }
                 if (fish_y < 0 || fish_y > SCREEN_H - fish_SIZE) {
                     duck_dy = -duck_dy;
+                    angle = 0.0;
+                    wall_touch = true;
                 }
-
+                if (fish_dx == 0 && angle < 3) {
+                    angle += .1;
+                }
+                if (fish_dx == (SCREEN_W - fish_SIZE) && angle >= 0) {
+                    angle -= .1;
+                }
+                if (angle < 3.14) {
+                    angle += 0.1; //pulled from Canvas assignment description
+                }
                 fish_x += fish_dx;
                 fish_y += duck_dy;
             }
@@ -86,6 +101,18 @@ int main(int argc, char** argv) {
             if (ev.keyboard.keycode == ALLEGRO_KEY_SPACE) {
                 pause = !pause; 
             }
+            else if (ev.keyboard.keycode == ALLEGRO_KEY_LEFT) {
+                flags |= ALLEGRO_FLIP_HORIZONTAL;
+            }
+            else if (ev.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
+                flags &= ~ALLEGRO_FLIP_HORIZONTAL;
+            }
+            else if (ev.keyboard.keycode == ALLEGRO_KEY_UP) {
+                flags |= ALLEGRO_FLIP_VERTICAL;
+            }
+            else if (ev.keyboard.keycode == ALLEGRO_KEY_DOWN) {
+                flags &= ~ALLEGRO_FLIP_VERTICAL;
+            }
         }
 
         if (redraw && al_is_event_queue_empty(event_queue)) {
@@ -95,7 +122,7 @@ int main(int argc, char** argv) {
                 al_draw_bitmap(ocean, 0, 0, 0);
             }
             if (fish) {
-                al_draw_scaled_bitmap(fish, 0, 0, al_get_bitmap_width(fish), al_get_bitmap_height(fish), fish_x, fish_y, fish_SIZE, fish_SIZE, 0);
+                al_draw_rotated_bitmap(fish, 16, 16, fish_x + 16, fish_y + 16, angle, flags);
             }
             al_flip_display();
         }
